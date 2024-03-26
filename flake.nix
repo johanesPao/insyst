856b5a266c99cb474b83f0cc102b8c5c3375b714
Johanes Gregorius Indra Pradana Pao <johanes.pao@gmail.com>
2024-03-26 13:28:21 +0700

    Penambahan direnv

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description: "Setup node.js dan environment pada project insyst";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_21
+          ];
+        };
+      }
+    );
+}
